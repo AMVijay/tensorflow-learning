652425d106e6c4235a42fecb159ba42b49282004
Vijay <AM.Vijay@gmail.com>
2019-11-04 10:21:24 -0600

    Python Installation Path Setup - Windows Script.

--- python-env-setup.cmd
+++ python-env-setup.cmd
@@ -1,7 +1,8 @@
+@echo OFF
+
 REM Vijay (AM.Vijay@gmail.com)
 
-@echo OFF
-SET PYTHON_HOME="C:\Users\135670\Vijay\Python\Python38"
+SET PYTHON_HOME=C:\Users\135670\Vijay\Python\Python38
 echo "PYTHON HOME Path is %PYTHON_HOME%"
 
 SET PATH=%PATH%;%PYTHON_HOME%;%PYTHON_HOME%\Scripts\;
